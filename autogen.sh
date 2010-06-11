2002a382d544e2a2a2746fc22c9cb3c1c22a6255
Alexandre Quessy <alexandre@quessy.net>
2010-06-11 14:18:17 -0400

    adding autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#if [ `uname -s` = 'Darwin' ]; then
+#  LIBTOOLIZE="glibtoolize"
+#else
+#  LIBTOOLIZE="libtoolize"
+#fi
+
+# could be replaced with autoreconf -fivI m4 (verbose, force rebuild of ltmain, .in files, etc.)
+#${LIBTOOLIZE} --force
+autoreconf --install
+
+if [ $? != 0 ]; then 
+    echo "autoreconf return value is $?"
+    exit 1
+fi
+
